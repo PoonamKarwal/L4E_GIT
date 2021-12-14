4e7aeff28490b2d54d0703cd9135a5bed02c022a
PoonamKarwal <poonamkarwal29@gmail.com>
2021-12-14 14:29:21 -0700

    Firstcommit

---
+++ src/main/java/com/newmaven/learn4earn/Learn4Earn_New_GitPushPull/App.java
@@ -0,0 +1,13 @@
+package com.newmaven.learn4earn.Learn4Earn_New_GitPushPull;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/newmaven/learn4earn/Learn4Earn_New_GitPushPull/AppTest.java
@@ -0,0 +1,20 @@
+package com.newmaven.learn4earn.Learn4Earn_New_GitPushPull;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
